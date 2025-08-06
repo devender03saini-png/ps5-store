c2b8496e4528e05de0750cb16388b1fe89e88ab9
Ddevender Saini <devender03saini@gmail.com>
2025-08-07 01:16:46 +0530

    message

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 $host = "localhost";
 $user = "root";
 $pass = "";
@@ -8,5 +8,18 @@ $conn = mysqli_connect($host, $user, $pass, $dbname);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
+}
+?> -->
+
+<?php
+$servername = "sql108.infinityfree.com"; // Check your account's SQL host
+$username = "if0_39649374";       // Your account username
+$password = "";    // The password you chose earlier
+$dbname = "if0_39649374_ps5_db";  // Your full database name
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
